52429fcd0b096cb30db0cf9010348369bf363d0a
SiqiLiu-Math <siqi.liu.math@gmail.com>
2026-09-14 12:35:58 -0400

    upload data and base code

---
+++ Model/graph_config.py
@@ -0,0 +1,158 @@
+
+state_names = {
+    0: "AL",
+    1: "AZ",
+    2: "AR",
+    3: "CA",
+    4: "CO",
+    5: "CT",
+    6: "DC",
+    7: "DE",
+    8: "FL",
+    9: "GA",
+    10: "ID",
+    11: "IL",
+    12: "IN",
+    13: "IA",
+    14: "KS",
+    15: "KY",
+    16: "LA",
+    17: "ME",
+    18: "MD",
+    19: "MA",
+    20: "MI",
+    21: "MN",
+    22: "MS",
+    23: "MO",
+    24: "MT",
+    25: "NE",
+    26: "NV",
+    27: "NH",
+    28: "NJ",
+    29: "NM",
+    30: "NY",
+    31: "NC",
+    32: "ND",
+    33: "OH",
+    34: "OK",
+    35: "OR",
+    36: "PA",
+    37: "RI",
+    38: "SC",
+    39: "SD",
+    40: "TN",
+    41: "TX",
+    42: "UT",
+    43: "VT",
+    44: "VA",
+    45: "WA",
+    46: "WV",
+    47: "WI",
+    48: "WY"
+}
+
+state_pos = {
+     0: (2,-2),   # AL
+     1: (-3,-1),  # AZ
+     2: (0,-1),   # AR
+     3: (-5,0),   # CA
+     4: (-2,0),   # CO
+     5: (6,1),    # CT
+     6: (4,-1),   # DC
+     7: (5,0),    # DE
+     8: (3,-3),   # FL
+     9: (3,-2),   # GA
+    10: (-3,1),   # ID
+    11: (1,1),    # IL
+    12: (2,1),    # IN
+    13: (0,1),    # IA
+    14: (-1,0),   # KS
+    15: (2,0),    # KY
+    16: (0,-2),   # LA
+    17: (7,3),    # ME
+    18: (4,0),    # MD
+    19: (6,2),    # MA
+    20: (2,2),    # MI
+    21: (0,2),    # MN
+    22: (1,-2),   # MS
+    23: (0,0),    # MO
+    24: (-2,2),   # MT
+    25: (-1,1),   # NE
+    26: (-4,0),   # NV
+    27: (6,3),    # NH
+    28: (5,1),    # NJ
+    29: (-2,-1),  # NM
+    30: (5,2),    # NY
+    31: (4,-2),   # NC
+    32: (-1,3),   # ND
+    33: (3,1),    # OH
+    34: (-1,-1),  # OK
+    35: (-4,1),   # OR
+    36: (4,1),    # PA
+    37: (7,1),    # RI
+    38: (4,-3),   # SC
+    39: (-1,2),   # SD
+    40: (2,-1),   # TN
+    41: (-1,-2),  # TX
+    42: (-3,0),   # UT
+    43: (5,3),    # VT
+    44: (3,-1),   # VA
+    45: (-4,2),   # WA
+    46: (3,0),    # WV
+    47: (1,2),    # WI
+    48: (-2,1),   # WY
+}
+
+state_abbr = {
+    "Alabama": "AL",
+    "Alaska": "AK",
+    "Arizona": "AZ",
+    "Arkansas": "AR",
+    "California": "CA",
+    "Colorado": "CO",
+    "Connecticut": "CT",
+    "Delaware": "DE",
+    "District of Columbia": "DC",
+    "Florida": "FL",
+    "Georgia": "GA",
+    "Hawaii": "HI",
+    "Idaho": "ID",
+    "Illinois": "IL",
+    "Indiana": "IN",
+    "Iowa": "IA",
+    "Kansas": "KS",
+    "Kentucky": "KY",
+    "Louisiana": "LA",
+    "Maine": "ME",
+    "Montana": "MT",
+    "Nebraska": "NE",
+    "Nevada": "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    "Ohio": "OH",
+    "Oklahoma": "OK",
+    "Oregon": "OR",
+    "Maryland": "MD",
+    "Massachusetts": "MA",
+    "Michigan": "MI",
+    "Minnesota": "MN",
+    "Mississippi": "MS",
+    "Missouri": "MO",
+    "Pennsylvania": "PA",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Utah": "UT",
+    "Vermont": "VT",
+    "Virginia": "VA",
+    "Washington": "WA",
+    "West Virginia": "WV",
+    "Wisconsin": "WI",
+    "Wyoming": "WY"
+}
